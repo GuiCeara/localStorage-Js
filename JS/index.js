5aee576a4a1923012655070c9f2672998d2bc151
GuiCeara <guilhermecororido@gmail.com>
2023-04-19 00:00:33 -0300

    Login/Cadastro funcionando corretamente!

--- JS/index.js
+++ JS/index.js
@@ -1,56 +1,22 @@
-let button = document.getElementById('submit')
-let buttonCad = document.querySelector('.text-cad')
-let confirmPasswordInput = document.querySelector('#confirmPassword')
-let inputs_text = document.querySelector('.inputs-text')
-let text = document.querySelector('.text')
-let text_cad = document.querySelector('.text-cad')
-let submit = document.querySelector('#submitCad')
-
-button.addEventListener('click', function menu() {
-    
-    let userName = document.getElementById('user').value
-    let userPassWord = document.getElementById('password').value
-    let usersList = JSON.parse(localStorage.getItem('users'))
-
-    if (userName == '' || userPassWord == '') {
-        alerts(text = 'Preencha os campos!', color = 'rgba(215,44,44,0.593)')
-    }else{
-        if (buscar(userName, userPassWord, usersList, verify = 1) == true){
-            alerts(text = 'Usuário Logado!', color = 'rgba(58, 215, 44, 0.593)')
-            // logar no site
-        }
-    }
-    
-})
-
-buttonCad.addEventListener('click', () => {
-    submit.addEventListener('click', () => {
-        // terminar
-    })
-    submit.style.display = 'block'
-    button.style.display = 'none'
-    text.innerHTML = 'Cadastrar'
-    text_cad.innerHTML = 'Logar-se'
-    confirmPasswordInput.style.display = 'inline'
-    inputs_text.style.marginTop = '100px'
-    
-})
+let usersList = JSON.parse(localStorage.getItem('users')) || []
+const user = document.getElementById('user')
+const pass = document.getElementById('password')
+const confirmPasswordInput = document.getElementById('confirmPassword')
 
 function verify(userName, userPassWord, usersList) {
-    if (!localStorage.getItem('users')){
-        cadastrar(userName, userPassWord)
+    if (usersList.length === 0){
+        cadastrar(userName, userPassWord, usersList)
     }else{
-        if (buscar(userName, userPassWord, usersList, verify = 0) == false){
+        if (buscar(userName, userPassWord, usersList, verifyN = 0) == false){
             if(cadastrar(userName, userPassWord, usersList) == true){
-                // logar no site
+                return true
+            }
+            else{
+                return false
             }
         }
         else{
-            const user = document.getElementById('user')
-            const pass = document.getElementById('password')
-            alerts(text = 'Usuário já existente', color = "rgba(215,44,44,0.593)")
-            user.value = ''
-            pass.value = ''
+            return false
         }
     }
 }
@@ -67,17 +33,17 @@ function cadastrar(userName, userPassWord, usersList = []) {
     return true
 }
 
-function buscar(userName, userPassWord, usersList, verify) {
+function buscar(userName, userPassWord, usersList, verifyN) {
     
     let user = usersList.find(i => i.user === userName)
     let userPass = usersList.find(i => i.password === userPassWord)
-    if (verify == 0){
+    if (verifyN == 0){
         if (user) {
             return true
         }
         return false
 
-    }else if (verify == 1) {
+    }else if (verifyN == 1) {
         if (user && userPass){
             return true
         }
@@ -93,6 +59,89 @@ function alerts(text, color) {
     alert_div.style.display = 'block'
     setInterval(() => {
         alert_div.style.display = 'none'
-    }, 2000)
+    }, 3000)
     alert_span.innerHTML = text
 }
+
+let button = document.getElementById('submit')
+let buttonCad = document.getElementById('text-cad')
+let buttonLog = document.getElementById('text-log')
+let inputs_text = document.querySelector('.inputs-text')
+let text = document.querySelector('.text')
+let submit = document.getElementById('submitCad')
+
+button.addEventListener('click', () => {
+
+    let userName = document.getElementById('user').value
+    let userPassWord = document.getElementById('password').value
+
+    if (userName == '' || userPassWord == '') {
+        alerts('Preencha os campos!', 'rgba(215,44,44,0.593)')
+    }else{
+        if (buscar(userName, userPassWord, usersList, verifyN = 1) == true){
+            alerts('Usuário Logado!', 'rgba(58, 215, 44, 0.593)')
+            // logar no site
+        }else{
+            alerts('Usuário ou senha incorretos...', 'rgba(215,44,44,0.593)')
+        }
+    }
+    
+})
+
+buttonCad.addEventListener('click', () => {
+
+    submit.style.display = 'block'
+    button.style.display = 'none'
+    buttonLog.style.display = 'block'
+    buttonCad.style.display = 'none'
+    text.innerHTML = 'Cadastrar'
+    confirmPasswordInput.style.display = 'inline'
+    inputs_text.style.marginTop = '100px'
+    user.value = ''
+    pass.value = ''
+
+})
+
+buttonLog.addEventListener('click', () => {
+
+    submit.style.display = 'none'
+    button.style.display = 'block'
+    buttonLog.style.display = 'none'
+    buttonCad.style.display = 'block'
+    text.innerHTML = 'Login'
+    confirmPasswordInput.style.display = 'none'
+    user.value = ''
+    pass.value = ''
+
+})
+
+submit.addEventListener('click', () => {
+
+    let userName = document.getElementById('user').value
+    let userPassWord = document.getElementById('password').value
+    let confirmPassword = confirmPasswordInput.value
+
+    if (userName == '' || userPassWord == '' || confirmPassword == '') {
+        alerts('Preencha os campos!', 'rgba(215,44,44,0.593)')
+    }
+    else{
+        if (pass.value !== confirmPasswordInput.value) {
+            alerts('As senhas não correspondem.', 'rgba(215,44,44,0.593)')
+        } 
+        else {
+            if (verify(userName, userPassWord, usersList) == true) {
+                alerts('Usuário Cadastrado!', 'rgba(58, 215, 44, 0.593)')
+            }
+            else{
+                alerts('Usuário já existente', 'rgba(215,44,44,0.593)')
+                confirmPasswordInput.value = ''
+                user.value = ''
+                pass.value = ''
+            }
+        }
+    }
+})
+
+
+
+
